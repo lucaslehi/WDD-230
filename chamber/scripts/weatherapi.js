361332d344f19a74fbc3e745de6b42d9dae8bd4e
Lucas Gomes <lucaslehi@byui.edu>
2023-06-29 16:29:36 -0600

    updates

--- chamber/scripts/weatherapi.js
+++ chamber/scripts/weatherapi.js
@@ -31,7 +31,27 @@ function displayResults(weatherData) {
   temperature.textContent = `${temperatureCelsius}°C`;
   cloudyText.textContent = description;
   windSpeed.textContent = `Wind speed: ${windSpeedValue} km/h`;
-  windChill.textContent = "Wind Chill: N/A";
+
+  let temperatureValue = parseFloat(temperatureCelsius);
+  let windSpeedValueFloat = parseFloat(windSpeedValue);
+  let windChillValue = calculateWindChill(
+    temperatureValue,
+    windSpeedValueFloat
+  );
+  windChill.textContent = `Wind Chill: ${windChillValue}°C`;
+}
+
+function calculateWindChill(temperature, windSpeed) {
+  if (temperature <= 10 && windSpeed > 4.8) {
+    const windChill =
+      13.12 +
+      0.6215 * temperature -
+      11.37 * Math.pow(windSpeed, 0.16) +
+      0.3965 * temperature * Math.pow(windSpeed, 0.16);
+    return windChill.toFixed(2);
+  } else {
+    return "N/A";
+  }
 }
 
 function getWeatherEmoji(iconCode) {
